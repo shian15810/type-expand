ebe90745bd7c8c140d63ba26b3f257753c423fe4
shian15810 <shian15810@gmail.com>
2020-11-17 07:03:43 +0800

    Install prettier

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # type-expand
+
 Utilities to expand TypeScript types shown by IntelliSense in Visual Studio Code.
---
+++ prettier.config.cjs
@@ -0,0 +1,11 @@
+// @ts-check
+
+/**
+ * @type {import('prettier').Options}
+ */
+const config = {
+  singleQuote: true,
+  trailingComma: 'all',
+};
+
+module.exports = config;
