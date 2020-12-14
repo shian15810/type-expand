361b93323f4545d08a659995370db80c6b48857f
shian15810 <shian15810@gmail.com>
2020-12-14 14:23:37 +0800

    Remove reliance on type-fest

--- index.d.ts
+++ index.d.ts
@@ -1,13 +1,11 @@
-import { Primitive } from 'type-fest';
+export type Expand<T> = T extends object
+  ? T extends infer O
+    ? { [K in keyof O]: O[K] }
+    : never
+  : T;
 
-export type Expand<T> = T extends Primitive
-  ? T
-  : T extends infer O
-  ? { [K in keyof O]: O[K] }
-  : never;
-
-export type ExpandDeep<T> = T extends Primitive
-  ? T
-  : T extends infer O
-  ? { [K in keyof O]: ExpandDeep<O[K]> }
-  : never;
+export type ExpandDeep<T> = T extends object
+  ? T extends infer O
+    ? { [K in keyof O]: ExpandDeep<O[K]> }
+    : never
+  : T;
